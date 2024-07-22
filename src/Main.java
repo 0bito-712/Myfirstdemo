f77c6e57d5a824393ab84e5349e35147492ca295
0bito-712 <mohiteraviraj0@gmail.com>
2024-07-22 22:09:13 +0530

    Initial commit for this project

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("obito");
         System.out.println("712");
+        System.out.println("Raviraj Mohite");
     }
 }
